8c4b00235ba51521488eb08659901e4aefcf927e
Justine Delsoir <justine.delsoir0709@gmail.com>
2023-05-11 20:53:44 +0200

    factorisation du script jv

--- assets/script.js
+++ assets/script.js
@@ -21,17 +21,16 @@ const slides = [
  arrow_right = document.querySelector('.arrow_right');
 
  let click = 0;
- 
 
  function carroussel(){
-	var image =document.querySelector('.banner-img');
+	let image =document.querySelector('.banner-img');
 	image.src=slides[click].image;
 	let banner = document.getElementById('banner');
-	let texte =banner.querySelector('p')
+	let texte =banner.querySelector('p');
 	texte.innerHTML=slides[click].tagLine;
   }; 
  
-  const element =document.querySelector('.dots');
+ const element =document.querySelector('.dots');
 	
 for ( let i = 0 ; i< slides.length ; i++){
 	const bubble = document.createElement ('div');
@@ -40,45 +39,40 @@ for ( let i = 0 ; i< slides.length ; i++){
 };
 
 const container = document.querySelector('.dots');
-let elements = Array.from(container.children);	
-elements[0].classList.add('dot_selected');
+let tab = Array.from(container.children);	
+
+tab[0].classList.add('dot_selected');
 
 arrow_left.addEventListener('click' , function(){
+
+tab[click].classList.remove('dot_selected');
+console.log('avant decrémentation',click);
+
  click--;
+ console.log('aprés decrémentation',click);
+
 	if (click < 0){
 		click=slides.length-1;		
-	}
-	carroussel();
-	elements[click].classList.add('dot_selected');
-	
+	};
+	console.log('aprés condition',click);
 
-	if(click == slides.length-1){
-		indexbubble=0;
-	}
-	else {
-		indexbubble=click+1;
-	}
-  elements[indexbubble].classList.remove('dot_selected');
+	tab[click].classList.add('dot_selected');
+	
+	carroussel();
 });
 
 arrow_right.addEventListener('click' , function(){
-	if (click >= slides.length-1){
-		click=-1;
-	}
-	click++;
 
+  tab[click].classList.remove('dot_selected');
+
+  click++;
+
+	if (click >= slides.length){
+		click=0;
+	}
     carroussel();
 
-	elements[click].classList.add('dot_selected');
-	let indexbubble;
-	if(click == 0){
-		indexbubble=slides.length-1;
-	}
-	else {
-		indexbubble=click-1;
-	}
-	console.log(indexbubble);
-	console.log(click);
-	elements[indexbubble].classList.remove('dot_selected');
+	tab[click].classList.add('dot_selected');
+
 });
 
